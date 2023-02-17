124b36fce66f3a50bb6d97c040ea413a3fa06c0d
Daurn <93579101+Daurn@users.noreply.github.com>
2023-02-17 13:58:58 +0100

    Add files via upload

---
+++ Terraform/main.tf
@@ -0,0 +1,73 @@
+﻿# Definition des ressources Azure
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.43.0"
+    }
+  }
+  required_version = "1.3.7"
+}
+
+provider "azurerm" {
+  features {}
+  tenant_id       = ""
+  subscription_id = ""
+}
+
+resource "azurerm_resource_group" "gaming_1" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+
+resource "azurerm_service_plan" "gaming_1" {
+  name                = var.asp_name
+  location            = azurerm_resource_group.gaming_1.location
+  resource_group_name = azurerm_resource_group.gaming_1.name
+  os_type             = "Windows"
+  sku_name            = var.asp_sku_name
+}
+
+resource "azurerm_windows_web_app" "gaming_1" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.gaming_1.name
+  location            = azurerm_service_plan.gaming_1.location
+  service_plan_id     = azurerm_service_plan.gaming_1.id
+
+  site_config {
+    application_stack {
+      dotnet_version = var.dotnet_version
+    }
+  }
+}
+
+resource "azurerm_mssql_server" "gaming_1" {
+  name                         = var.db_server_name
+  resource_group_name          = azurerm_resource_group.gaming_1.name
+  location                     = azurerm_resource_group.gaming_1.location
+  version                      = "12.0"
+  administrator_login          = var.db_login
+  administrator_login_password = var.db_password
+  // TODO Add firewall rule to allow connection from Visual Studio
+}
+
+resource "azurerm_mssql_database" "gaming_1" {
+  name           = var.db_name
+  server_id      = azurerm_mssql_server.gaming_1.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 1
+  read_scale     = false
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+# The Azure feature Allow access to Azure services can be enabled by setting start_ip_address and end_ip_address to 0.0.0.0
+resource "azurerm_sql_firewall_rule" "gaming_1" {
+  name                = "FirewallRule1"
+  resource_group_name = azurerm_resource_group.gaming_1.name
+  server_name         = azurerm_mssql_server.gaming_1.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,19 @@
+﻿# Valeurs des variables
+
+# Resource Group
+rg_name     = "rg-gaming-001"
+rg_location = "France Central"
+
+# App Service Plan 
+asp_name     = "asp-gaming-002"
+asp_sku_name = "B1"
+
+# Windows Web App
+app_name       = "SnakeArnaudNic"
+dotnet_version = "v7.0"
+
+# DB
+db_server_name = "vm-managerv2-an"
+db_name        = "vm-databasev2-an"
+db_login       = "dodv2o"
+db_password    = "4-v3ry-53cr37-p455w0rd"
